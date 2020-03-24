0d71f2fdb144fce357ddacf9749429ff3caa499b
eclimov <klimovedik88@gmail.com>
2020-03-24 23:03:00 +0200

    - Updated jobs' schedule

--- src/Application.py
+++ src/Application.py
@@ -10,11 +10,11 @@ channel = Channel()
 # https://medium.com/better-programming/introduction-to-apscheduler-86337f3bb4a6
 sched = BackgroundScheduler(daemon=True, timezone=utc)  # Scheduler object
 
-sched.add_job(channel.telegram_send_advice, 'cron', hour='5-20/3', minute='1')
-sched.add_job(channel.telegram_send_image, 'cron', hour='6-21/4', minute='32')
-sched.add_job(channel.telegram_send_poll, 'cron', hour='5-20/7', minute='25')
-sched.add_job(channel.telegram_send_statistics_summary, 'cron', hour='7', minute='35')
-sched.add_job(channel.telegram_send_statistics_by_countries, 'cron', hour='6,12,20', minute='55')
+sched.add_job(channel.telegram_send_advice, 'cron', hour='5-20/5', minute='1')
+sched.add_job(channel.telegram_send_image, 'cron', hour='6-21/6', minute='32')
+sched.add_job(channel.telegram_send_poll, 'cron', hour='16', minute='25')
+sched.add_job(channel.telegram_send_statistics_summary, 'cron', hour='12', minute='35')
+sched.add_job(channel.telegram_send_statistics_by_countries, 'cron', hour='6,20', minute='55')
 sched.start()
 
 app = Flask(__name__)
