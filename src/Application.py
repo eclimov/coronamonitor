61d91a3745a33177a17a71ec3029d5a343fa1bca
eclimov <klimovedik88@gmail.com>
2020-04-22 21:07:33 +0300

    - Updated schedule

--- src/Application.py
+++ src/Application.py
@@ -21,9 +21,9 @@ def listener_scheduler_error(event: JobExecutionEvent):
     channelDebug.telegram_send_text(f'ERROR\n{str(event.exception)}\n{event.traceback}')
 
 
-sched.add_job(channel.telegram_send_statistics_by_countries, 'cron', hour='17', minute='55')
+sched.add_job(channel.telegram_send_statistics_by_countries, 'cron', hour='17', minute='10')
 sched.add_job(channel.telegram_send_statistics_summary, 'cron', hour='10', minute='35')
-sched.add_job(channel.telegram_send_image, 'cron', hour='8', minute='32')
+sched.add_job(channel.telegram_send_image, 'cron', hour='7', minute='32')
 # sched.add_job(channel.telegram_send_advice, 'cron', hour='10,17', minute='1')
 # sched.add_job(channel.telegram_send_poll, 'cron', hour='15', minute='25')
 
