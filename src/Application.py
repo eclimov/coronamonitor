20d78739491c84f31ce36c3adb5fb5e450337c29
eclimov <klimovedik88@gmail.com>
2022-01-29 00:50:48 +0200

    - Updated schedule

--- src/Application.py
+++ src/Application.py
@@ -22,8 +22,8 @@ def listener_scheduler_error(event: JobExecutionEvent):
 
 
 sched.add_job(channel.telegram_send_statistics_by_countries, 'cron', hour='6', minute='10')
-sched.add_job(channel.telegram_send_statistics_summary, 'cron', hour='15', minute='35')
-sched.add_job(channel.telegram_send_image, 'cron', day='*/2', hour='1', minute='32')
+sched.add_job(channel.telegram_send_statistics_summary, 'cron', hour='9', minute='35')
+sched.add_job(channel.telegram_send_image, 'cron', day='*/2', hour='18', minute='32')
 # sched.add_job(channel.telegram_send_advice, 'cron', hour='10,17', minute='1')
 # sched.add_job(channel.telegram_send_poll, 'cron', hour='15', minute='25')
 
